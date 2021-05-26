14ccb70327c9ab4a53955452291c2c627605aaf5
SzczesnaAg <agata.szczesna89@gmail.com>
2021-05-26 14:56:04 +0200

    reusable card component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            < CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Nice blog post." avatar={faker.image.avatar()} />
-            < CommentDetail author="Jane" timeAgo="Today at 2:00AM" text="I like it." avatar={faker.image.avatar()} />
-            < CommentDetail author="Alex" timeAgo="Yesterday at 5:00PM" text="Great blog!!" avatar={faker.image.avatar()} />
-            < CommentDetail author="John" timeAgo="Yesterday at 3:00PM" text="Love it <3" avatar={faker.image.avatar()} />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    <p>Are you sure you want to do this?</p>
+                </div>
+            </ApprovalCard>
+
+            < ApprovalCard>
+                < CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Nice blog post." avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            < ApprovalCard>
+                < CommentDetail author="Jane" timeAgo="Today at 2:00AM" text="I like it." avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                < CommentDetail author="Alex" timeAgo="Yesterday at 5:00PM" text="Great blog!!" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                < CommentDetail author="John" timeAgo="Yesterday at 3:00PM" text="Love it <3" avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 };
